76f3243fc77c75995dea2c3b0aeda38458e96abf
hongshuochen <hongshuo@usc.edu>
2020-08-26 18:59:58 -0700

    add PrintIntro

--- src/lib/cpplib.cc
+++ src/lib/cpplib.cc
@@ -1,7 +1,29 @@
 #include "cpplib.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 #include "limits"
 std::string CPPLib::PrintHelloWorld() { return "**** Hello World ****"; }
+std::string CPPLib::PrintIntro() { 
+  string line;
+  ifstream myfile ("/Users/max/Desktop/EE599/EE599_Lab1/pikachu.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line))
+    {
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+  return "**** Self-Introdction ****\n"
+          "Name: Hong-Shuo Chen\n" 
+          "Major: EE\n"
+          "Hobby: Cooking\n";
+}
+
 
 // Calculates the Nth Fibonacci number
 int CPPLib::fib(int N) {
